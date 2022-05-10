62f8001a29da96d8b8ea5cfe6222026edb2af877
Pavel Syomin <pas.ntg@gmail.com>
2022-05-10 13:17:58 +0500

    Refactor code, add options, add updater

--- intextads.php
+++ intextads.php
@@ -1,11 +1,16 @@
 <?php
+/**
+ * @package     Intext Ads, a Joomla plugin
+ *
+ * @author      Pavel Syomin
+ * @copyright   Copyright © Pavel Syomin, 2022 
+ * @license     GNU General Public License version 3; see LICENSE.txt
+ */
 
 // No direct access to this file
 defined('_JEXEC') or die();
 
 use Joomla\CMS\Plugin\CMSPlugin;
-use Joomla\Event\Event;
-use Joomla\Event\SubscriberInterface;
 
 class plgContentIntextads extends CMSPlugin
 {
@@ -13,18 +18,28 @@ class plgContentIntextads extends CMSPlugin
 	protected $app;
 
     public function onContentPrepare($context, &$row, &$params, $page = 0)
-    {        
+    {   
+        // Do not show ads in API mode
         if ($this->app->isClient('api'))
         {
             return;
         }
         
+        // Show only in articles
         if ($context == 'com_content.article')
-        {    
+        {
+            // Skip articles with a snippet disabling ads
+            if (strpos($row->text, '{intextads:off}') !== false
+                or strpos($row->text, '{intextads: off}') !== false)
+            {
+                return;
+            }
+            
+            // Get ad blocks from plugin settings
             $blocks = [];
             for ($i == 1; $i <= 5; $i++)
             {
-                $block = trim($this->params->get("block_$i", ""));
+                $block = trim($this->params->get("block$i", ""));
                 if ($block != '')
                 {
                     $blocks[] = $block;
@@ -35,61 +50,73 @@ class plgContentIntextads extends CMSPlugin
             {
                 return;
             }
-                        
+            
+            // Place blocks in article text
             $this->_place($blocks, $row);
+            
+            // Add custom CSS to document head
+            $this->_addCSS();
         } 
 	}
     
-    protected function _place($blocks, &$row)
+    private function _place($blocks, &$row)
     {
-        $text_length = strlen($row->text);
-        if ($text_length <= 25000)
+        // Calculate the number of blocks to add
+        $textLength = strlen($row->text);
+        if ($textLength <= 25000)
         {
-            $max_n_blocks = intdiv($text_length, 5000);
+            $maxNBlocks = intdiv($textLength, 5000);
         }
         else
         {
-            $max_n_blocks = 5;
+            $maxNBlocks = 5;
         }
-        $n_blocks = min(count($blocks), $max_n_blocks);
+        $nBlocks = min(count($blocks), $maxNBlocks);
         
-        if ($n_blocks == 0)
+        if ($nBlocks == 0)
         {
             return;
         }
         
-        $n_paragraphs = substr_count($row->text, '<p');
-        $start_offset = 5;
-        $end_offset = 10;
-        $n_paragraphs_avail = $n_paragraphs - $start_offset - $end_offset;
-        $blocks_freq = intdiv($n_paragraphs_avail, $n_blocks);
+        // Calculate blocks frequency
+        $nParagraphs = substr_count($row->text, '<p');
+        $startOffset = $this->params->get('startOffset', 5);
+        $endOffset = $this->params->get('endOffset', 10);
+        $nParagraphsAvail = $nParagraphs - $startOffset - $endOffset;
+        $blocksFreq = intdiv($nParagraphsAvail, $nBlocks);
         
-        if ($n_paragraphs < 5 || $n_paragraphs_avail < 0)
+        if ($nParagraphs < 5 || $nParagraphsAvail < 0)
         {
             return;
         }
         
+        // Add blocks after paragraphs with calculated position
         $pos = strpos($row->text, '<p');
         $count = 0;
+        $blocksClass = $this->params->get('blocksClass', 'intext-ads');
         while ($pos !== false)
         {
-            $offset = $pos + 2;
+            $offset = $pos + strlen('<p');
             $pos = strpos($row->text, '<p', $offset);
             $count++;
-            if ($count == $start_offset)
+            if ($count == $startOffset)
             {
                 $block = array_shift($blocks);
-                $block = "<div class=\"intext-ad\">\n" . $block . "\n</div>\n";
+                $block = "<div class=\"$blocksClass\">\n" . $block . "\n</div>\n";
                 $row->text = substr_replace($row->text, $block, $pos, 0);
-                $start_offset += $blocks_freq;
-                $n_blocks--;
-                if ($n_blocks == 0)
+                $startOffset += $blocksFreq;
+                $nBlocks--;
+                if ($nBlocks == 0)
                 {
                     break;
                 }
             }
-        }
-        
-        $this->app->getDocument()->addStyleDeclaration('.intext-ad{margin-bottom: 1rem;}');     
+        }        
+    }
+    
+    private function _addCSS()
+    {
+        $css = $this->params->get('css', '');
+        $this->app->getDocument()->addStyleDeclaration($css);     
     }
 }
